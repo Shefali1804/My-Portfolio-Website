8e86c9c2e6fc96df4f0efc27fc4e9deb723c5b6a
Shefali Gupta <shefaligupta1804@gmail.com>
2023-09-18 18:14:44 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-var tablinks=document.getElementsByClassName("tab-links");
-var tabcontents=document.getElementsByClassName("tab-contents");
-function opentab(tabname){
-    for(tablink of tablinks){
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
-    for(tabcontent of tabcontents){
+    for (tabcontent of tabcontents) {
         tabcontent.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab")
 }
 
-var sidemenu=document.getElementById("sidemenu");
-function openmenu(){
-    sidemenu.style.right="0";
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
 }
-function closemenu(){
-    sidemenu.style.right="-340px";
+function closemenu() {
+    sidemenu.style.right = "-200px";
 }
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycby_yQjrCHxGE5YEwdPizoZAwLO8Ka-19r6Jtep46n2bJHZtEEWi8dbKlOgWaE0StsnL/exec'
+
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
+
